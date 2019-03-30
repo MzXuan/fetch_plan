c5a2332042e2aac59cac2be9d987c005a2c8de84
X <wendyxuanz@gmail.com>
2019-03-30 17:52:34 +0800

    add a cycle for training

--- baselines/baselines/ppo2/ppo2.py
+++ baselines/baselines/ppo2/ppo2.py
@@ -129,17 +129,17 @@ class Runner(object):
             # # ----end debug----#
 
             #---- todo: add predict reward
-            # # predict_weight =0.0005
-            # if self.predictor_flag:
-            #     self.predictor.predict(self.obs[:], self.dones,
-            #                                self.env.mean, self.env.var)
-            #     # print("squred loss: ")
-            #     # print(np.square(predict_loss))
-            #     # rewards = -predict_loss*np.square(predict_weight)+rewards
-            # else:
-            #     self.predictor.collect(self.obs[:], self.dones)
-            #     # self.predictor.collect(self.obs[:], self.dones,
-            #     #                        self.env.ob_rms.mean, self.env.ob_rms.var)
+            predict_weight = 1
+            if self.predictor_flag:
+                predict_loss = self.predictor.predict(self.obs[:], self.dones,
+                                           self.env.mean, self.env.var)
+                # print("squred loss: ")
+                # print(np.square(predict_loss))
+                rewards -= predict_loss*np.square(predict_weight)+rewards
+            else:
+                self.predictor.collect(self.obs[:], self.dones)
+                # self.predictor.collect(self.obs[:], self.dones,
+                #                        self.env.ob_rms.mean, self.env.ob_rms.var)
 
             mb_rewards.append(rewards)
 
---
+++ baselines/baselines/ppo2/train_cycle.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+trap finish SIGINT
+
+function finish() {
+        echo "bye bye!"
+        exit
+}
+
+rl_model="./log"
+pred_model="./pred"
+
+
+counter=0
+while [ $counter -le 10 ]
+do
+echo $counter
+# copy saved file and rename
+
+cp -R ${rl_model} "./models/log_${counter}"
+cp -R ${pred_model} "./models/pred_${counter}"
+
+# run new training cycle
+python run.py -t=True -l=True
+python run.py -l=True
+python predictor.py
+
+((counter++))
+
+
+done
+echo All done
