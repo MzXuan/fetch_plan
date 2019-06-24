c2eab2cd5b069e1a312cffcfd469b2997c8efc7f
X <wendyxuanz@gmail.com>
2019-06-24 15:17:29 +0800

    change batchsiza

--- baselines/baselines/ppo2/predictor_new.py
+++ baselines/baselines/ppo2/predictor_new.py
@@ -326,7 +326,7 @@ if __name__ == '__main__':
     if not os.path.isdir("./pred"):
         os.mkdir("./pred")
 
-    rnn_model = Predictor(1024, out_steps, train_flag=True, epoch=args.epoch,
+    rnn_model = Predictor(64, out_steps, train_flag=True, epoch=args.epoch,
                           iter_start=args.iter, lr=args.lr, load=args.load, model_name="{}_{}_seq_tanh".format(NUM_UNITS, NUM_LAYERS))
 
     # rnn_model.plot_dataset()
