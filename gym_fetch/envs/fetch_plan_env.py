017d4c8721c2a1a342fe1225c8ad0e47e28210dd
X <wendyxuanz@gmail.com>
2019-09-09 15:36:02 +0800

    update env

--- gym_fetch/envs/fetch_plan_env.py
+++ gym_fetch/envs/fetch_plan_env.py
@@ -85,7 +85,8 @@ class FetchPlanEnv(fetch_LSTM_reward_env.FetchLSTMRewardEnv, utils.EzPickle):
         goal = np.zeros(3)
         goal[0] = table_pose[0] + (2 * np.random.uniform() - 1) * (
                     table_size[0] - self.sim.model.site_size[index_site].reshape(3, )[0])
-        goal[1] = table_pose[1] + (2 * np.random.uniform() - 1) * table_size[1]
+        goal[1] = table_pose[1] + (2 * np.random.uniform() - 1) * (
+                    table_size[1]- self.sim.model.site_size[index_site].reshape(3, )[1])
         goal[2] = table_pose[2] + table_size[2] + self.sim.model.site_size[index_site].reshape(3, )[
             2]  # deduce radius
         return goal.copy()
